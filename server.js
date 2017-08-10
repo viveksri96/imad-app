4a5a6ff11adff73a3da64f9a2613e4cd38386a87
viveksri96 <vivsri95@gmail.com>
2017-08-11 03:55:12 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -28,14 +28,7 @@ app.get('/ui/madi.png', function (req, res) {
 
 var pool = new Pool(config);
 app.get('/test-db',function (req, res) {
-    pool.query('SELECT * FROM user',function(err,result){
-        if(err){
-            res.status(500).send(err.toString());
-        }
-        else{
-            res.send(JSON.stringify(result));
-        }
-    });
+    res.send('test db');
 });
 
 
