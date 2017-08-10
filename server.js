23d7bb56bc0b94e0e7eac452ede5bd27ac828a98
viveksri96 <vivsri95@gmail.com>
2017-08-11 04:38:12 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.get('/test-db',function (req, res) {
     console.log(pool);
     pool.query('SELECT * FROM test',function(err,result){
         if(err){
-            res.status(500).send(err.toString());
+            console.log('ege');
         }
         else{
             res.send(JSON.stringify(result));
